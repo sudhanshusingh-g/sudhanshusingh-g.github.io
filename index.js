0929dccaea4340b9c1671204822032bfad02da3c
Sudhanshu Singh <sudsingh32@gmail.com>
2023-10-13 23:46:53 +0530

    Menu reponsiveness and project overlay effect has been done.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const toggleMenu=document.getElementById("menuBar");
 
 const menuDisplay=document.getElementById("menuDisplay");
 
+const menuItem=document.querySelector(".menu-item");
 toggleSwitch.addEventListener("click",()=>{
     body.classList.toggle("dark")
     darkModeIcon.classList.toggle("fa-moon");
@@ -13,19 +14,21 @@ toggleSwitch.addEventListener("click",()=>{
 })
 
 toggleMenu.addEventListener("click",()=>{
-    if(document.getElementsByClassName("fa-bars")){
+    if(toggleMenu.classList.contains("fa-bars")){
         toggleMenu.classList.add("fa-close")
         toggleMenu.classList.remove("fa-bars");
         menuDisplay.style.left='0';
     }
     else{
-        toggleMenu.classList.add("fa-bars")
         toggleMenu.classList.remove("fa-close");
+        toggleMenu.classList.add("fa-bars");
         menuDisplay.style.left='-100%';
     }
     
-   
 })
+menuItem.onclick=function(e){
+    e.stopPropagation();
+}
 
 
 
