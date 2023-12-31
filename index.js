9c8b8e65229b89de69ae2f3751fc2bb1b8d6c3e4
Sudhanshu Singh <sudsingh32@gmail.com>
2023-12-31 18:11:24 +0530

    updating changes

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const body=document.querySelector('body');
-const themeBtn=document.getElementById('theme')
-themeBtn.addEventListener("click",()=>{
-    body.classList.toggle('dark')
-    themeBtn.classList.toggle('fa-moon')
-})
-
-let currTime=new Date()
-console.log(currTime.getHours());
-
-if(currTime.getHours()>=18 && currTime.getHours()<=23 || currTime.getHours()>=0 && currTime.getHours<=6){
-    body.classList.add('dark')
-}
+// const body=document.querySelector('body');
+// const themeBtn=document.querySelector('.themeBtn')
+// themeBtn.addEventListener("click",()=>{
+//     body.classList.toggle('dark')
+//     themeBtn.classList.toggle('fa-moon')
+// })
